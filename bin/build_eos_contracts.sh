f160784ac984435ad45054795278624b159d2728
Eric Passmore <eric.passmore@gmail.com>
2024-08-19 19:05:46 -0700

    old v3.5 contracts work with leap

--- bin/build_eos_contracts.sh
+++ bin/build_eos_contracts.sh
@@ -28,7 +28,7 @@ git checkout $CONTRACTS_GIT_COMMIT_TAG
 git pull origin $CONTRACTS_GIT_COMMIT_TAG
 mkdir build
 cd build || exit
-cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=ON -Dspring_DIR="${SPRING_BUILD_DIR}/lib/cmake/spring" ..
+cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_TESTS=OFF ..
 #>> "${LOG_DIR}"/system_contracts_build.log 2>&1
 make -j ${NPROC}
 #>> "${LOG_DIR}"/system_contracts_build.log 2>&1
