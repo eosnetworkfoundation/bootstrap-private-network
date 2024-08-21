c78cdabee5b73188d31877b2402612d40935a568
Eric Passmore <eric.passmore@gmail.com>
2024-08-20 19:43:50 -0700

    updated attemps at msig

--- transaction/cleos-msig.sh
+++ transaction/cleos-msig.sh
@@ -1,23 +1,51 @@
 #!/bin/env bash
 
+CONTRACT_DIR="/local/eosnetworkfoundation/repos/eos-system-contracts/build/contracts"
+PUBLIC_SIG_KEY=EOS81nrWtjvMfDi9E7ddb5nbub2hBWWg6Kih7Y5oTuNPFv5mE72zN
+CHAIN_ID=493a38dc7c172cd8157fcfa4cc90ae955b3d2f714eba4dba959d5649e39efc15
+ENDPOINT=http://jungle4.cryptolions.io:80
+
+# create list of permissions
+# pull all the permissions from eosio.prods
+# create a new json array from the accounts
+printf '[' > /tmp/requested.json
+cleos -u $ENDPOINT get account eosio.prods -j \
+       | jq .permissions[].required_auth.accounts[].permission \
+       | sed 's/}/},/' >> /tmp/requested.json
+LINES=$(cat /tmp/requested.json | wc -l)
+let LINES=LINES-1
+head -${LINES} /tmp/requested.json > /tmp/perms2.json
+echo '}]' >> /tmp/perms2.json
+mv /tmp/perms2.json /tmp/requested.json
+
+
 # use cleos to create the multi-sig
-# second line  json is requstor my block producer account
-# thirs line json  is permission for transaction
-# fourth line is the action 
-cleos multisig propose testfeaturee \
-'[{"actor": "bpa", "permission": "active"}, {"actor": "bpb", "permission": "active"}, {"actor": "bpc", "permission": "active"}]' \
-      '[{"actor": "eosio", "permission": "active"} ]' \
-      eosio activate '{"feature_digest":"cbe0fafc8fcc6cc998395e9b6de6ebd94644467b1b4a97ec126005df07013c52"}' \
-      -p bpa@active
+# second line  json is requstor for my producer
+# third line json  is permission for transaction
+# fourth line is the action
+cleos -u $ENDPOINT multisig propose \
+    jung.svn.1 \
+    /tmp/requested.json \
+    '[{"actor": "eosio", "permission": "active"} ]' \
+    eosio activate '{"feature_digest":"cbe0fafc8fcc6cc998395e9b6de6ebd94644467b1b4a97ec126005df07013c52"}' \
+    -p spaceranger1@active
 
-cleos multisig approve bpa testfeatured \
-  '{"actor": "bpa", "permission": "active"}' \
-  -p bpa@active
-cleos multisig approve bpa testfeatured \
-    '{"actor": "bpb", "permission": "active"}' \
-    -p bpb@active
-cleos multisig approve bpa testfeatured \
-    '{"actor": "bpc", "permission": "active"}' \
-    -p bpc@active
+## EXample Approve and Execut in cleos
+#cleos multisig approve spaceranger1 testfeaturee \
+#  '{"actor": "spaceranger1", "permission": "active"}' \
+#  -p spaceranger1@active
+#
+# cleos multisig exec spaceranger1 testfeaturee -p spaceranger1@active
 
-cleos multisig exec bpa testfeatured -p bpa@active
+## SETUP EOSC
+# DOWNLOAD RELEASE https://github.com/eoscanada/eosc/releases
+# IMPORT KEYS eosc vault create --import
+
+# Prepare unsigned setcode setabi transaction
+
+cleos set contract eosio ${CONTRACT_DIR}/eosio.system eosio.system.wasm eosio.system.abi -s -d \
+    --json-file ${CONTRACT_DIR}/setcontract-eosio.system.json --expiration 8640000
+
+eosc -u $ENDPOINT multisig propose spaceranger1 testset.1 \
+    ${CONTRACT_DIR}/setcontract-eosio.system.json \
+    --request-producers
