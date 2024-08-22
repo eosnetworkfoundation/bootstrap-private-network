5e275a8b3d5ef61c046e7b3ed39ea66f53563634
Eric Passmore <eric.passmore@gmail.com>
2024-08-22 13:29:13 -0700

    don't activate features not active on mainnet

--- diagnostics/list_features.sh
+++ diagnostics/list_features.sh
@@ -1,13 +1,13 @@
 #!/bin/env bash
 
 COMMAND=${1:-"UNACTIVATED"}
-
+ENDPOINT=${2:-http://127.0.0.1:8888}
 # Get all avalible features
 
-curl -X POST http://127.0.0.1:8888/v1/producer/get_supported_protocol_features \
+curl -X POST ${ENDPOINT}/v1/producer/get_supported_protocol_features \
 | jq -r '.[] | "\(.specification[].value) \(.feature_digest)"' | sort > /tmp/all_features.txt
 
-curl -X POST http://127.0.0.1:8888/v1/chain/get_activated_protocol_features \
+curl -X POST ${ENDPOINT}/v1/chain/get_activated_protocol_features \
 | jq -r '.activated_protocol_features[] | "\(.specification[].value) \(.feature_digest)"' | sort > /tmp/active_features.txt
 
 if [ $COMMAND == "UNACTIVATED" ]; then
