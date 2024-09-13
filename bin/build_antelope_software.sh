ba4d88ee97e0c227d610a356afb9df66b0aa3a5a
Eric Passmore <eric.passmore@gmail.com>
2024-09-13 14:09:48 -0700

    updates to latest spring releases

--- bin/build_antelope_software.sh
+++ bin/build_antelope_software.sh
@@ -6,7 +6,7 @@
 # called from Docker Build
 ###
 
-SPRING_GIT_COMMIT_TAG=${1:-v1.0.0}
+SPRING_GIT_COMMIT_TAG=${1:-v1.0.1}
 CDT_GIT_COMMIT_TAG=${2:-v4.1.0}
 NPROC=${3:-$(nproc)}
 TUID=$(id -ur)
--- master-script-non-producer.sh
+++ master-script-non-producer.sh
@@ -7,9 +7,9 @@ curl -X POST ${ENDPOINT}/v1/producer/create_snapshot > /local/eosnetworkfoundati
 cat /local/eosnetworkfoundation/snapshot.json | jq
 SNAPSHOT=“”
 # Install Spring
-curl -L --output /local/eosnetworkfoundation/antelope-spring_1.0.0_amd64.deb https://github.com/AntelopeIO/spring/releases/download/v1.0.0/antelope-spring_1.0.0_amd64.deb 2> /dev/null
+curl -L --output /local/eosnetworkfoundation/antelope-spring_1.0.1_amd64.deb https://github.com/AntelopeIO/spring/releases/download/v1.0.1/antelope-spring_1.0.1_amd64.deb 2> /dev/null
 mkdir /local/eosnetworkfoundation/spring
-dpkg -x /local/eosnetworkfoundation/antelope-spring_1.0.0_amd64.deb /local/eosnetworkfoundation/spring
+dpkg -x /local/eosnetworkfoundation/antelope-spring_1.0.1_amd64.deb /local/eosnetworkfoundation/spring
 
 # Shutdown
 ps -u enfuser -f | grep eosbproducer
